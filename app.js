ae006dec180ebe4fc58954d896a072ea837bfb96
unknown <adamolarenwaju355@gmail.com>
2024-11-18 10:38:33 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,150 @@
+
+// var x =5;
+// let y= 5;
+
+// const z = x + y;
+
+// console.log(z)
+
+// var a = 5;
+// const b = 5;
+// c=5;
+
+// let c = a+ b;
+// console.log(c)
+
+// let firstname ="Yusuf"
+// let lastname = "Olarenwaju";
+// let age= "23"
+// let address="32 solomade garden "
+
+// console.log(firstname)
+// console.log(lastname)
+// console.log(age) 
+// console.log(address)
+
+// var phonename="Samsung Galaxy S24 ultra"
+// var phoneamount="2.5M"
+// console.log=(phonename+phoneamount)
+
+// let numbers= ["15","23","32"]
+// console.log(numbers)
+
+// content Arrays
+// const cars = ["Benz","Jeep","Toyota"]
+// cars.push("Mclaren")
+// console.log(cars)
+
+// var a=12;
+// var b=3;
+// var c=5;
+// var d=6;
+// var e=20;
+// let f=(((a+b-c)*d)/e)
+// console.log(f)
+
+// let red=18;
+// let blue=17;
+
+// if (red>blue){
+//     console.log("you can enter")
+// }else{
+//     console.log("you can't enter")
+
+
+
+// let w=1000000000
+// let a=500
+// if (a>w){
+//     console.log("Money dey")
+// }else{
+//     console.log("Account red")
+// }
+
+// const fruits = ["Apple","Banana","mango","Pineapple"];
+// let b = fruits.length;
+
+// for(let i = 0; i<5; i++){
+//     console.log(i)
+// }
+
+// const bank = {fname:"Solomon", lname:"Olayiwola" , age:"17", account:"$10909930093498599898979869456476347869846956764786745675875498649858752836487600990976545678996498659854"}
+
+// let text ="";
+
+// for(let x in bank){
+//     console.log(bank[x])
+// }
+
+// const number=[50,70,89,15,16]
+// let text=""
+// for(let x in number){
+//     console.log(number[x])
+// }
+
+// for(let i=70; i<=100; i++){
+//     console.log(i)
+// }
+
+// let count1=1;
+// do{
+//     console.log(count1)
+//     count1++;
+
+// }while(count1<=50);
+
+// Functions in Javascripts
+
+// function addTwoNumbers(a,b){
+//     return a+b;
+    
+// }
+// console.log(addTwoNumbers(10,10))
+
+// function Bi(Oyekanmiadam){
+
+//     return Bi; 
+// function calculateareaofsquare(side){
+//     return side + side
+// }
+// const sidelength =5;
+// const area = calculateareaofsquare(sidelength)
+// console.log("the area of the square is: (25)");
+// const text = "Aptech Nigeria";
+// const numberOfCharacters = text.length;
+
+// const text = "Aptech Nigeria";
+// const numberOfCharacters = text.length;
+
+// console.log(numberOfCharacters); // Outputs: 14
+// const capitalText = "HELLO WORLD";
+// const smallText = capitalText.toLowerCase();
+
+// console.log(smallText);
+// const originalText = "HELLO WORLD";
+// const reversedText = originalText.split('').reverse().join('');
+
+// console.log(reversedText);
+
+// let address="ONE TESLIM BALOGUN STADIUM";
+// let combined=address.split(" ").join("");
+// console.log(combined)
+// let da=  new Date;
+// console.log(da)
+
+// const gadget = new Map();
+// -
+// gadget.set("Macbook Air", 1500000);
+// gadget.set("Macbook Pro", 1300000);
+// gadget.set("Acer" , 200000);
+// gadget.set("Lenovo" , 400000);
+// gadget.set("HP" , 4890000);
+
+
+// console.log(gadget)
+
+var  A= "Monday is the begiing of the week";
+var B = "Monday always comes with hustle";
+console.log(A);
+console.log(B); 
+
